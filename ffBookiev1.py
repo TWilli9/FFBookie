99889bfc164fe010b39f77157f2f510f207d7790
theowilliams <theowilliams@uiowa.edu>
2025-02-18 19:23:15 -0600

    2/18

---
+++ ffBookiev1.py
@@ -0,0 +1,17 @@
+from espn_api.football import League
+#import pandas as pd
+#import dash
+#from dash import html, dcc, dash_table
+#import plotly.graph_objects as go
+#from random import choice  # For coin flip tiebreakers
+
+# Initialize league connection with your provided credentials
+league = League(
+    league_id=42024189,
+    year=2024,
+    espn_s2='AECycM1RSKC9H6KO4Qw7b0ZKIaa417A48498axeqW12XB0VFyWXroqy%2BFzAdJFMJUqxu4t05etxquUZYQ92C7V8N%2BzGT48zFtm4IJM04CG%2FG7zrSRXMBsqrw219pF4k7L0BYwwHr1om5AQNTKViQ5YJhH9SFEmGo03L1NTeuQSPy3Ws6HpQs2pfnZKuddHWxNUwH9HVOxVkOc4nSbCn8LPm2c1lsCAuuH26Z4laiqV2e0MCjMNizTi%2FS8VFHmVCXcPVejE3reh1JRyiHuKp1gE14',
+    swid='{CDA2BA80-43BE-41FB-9AB1-C8BE52DD4C45}'
+)
+
+print(league.teams)
+print(league.scoreboard())
